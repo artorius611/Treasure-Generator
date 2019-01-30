2175006c9630c9d9ca92546ce7d051453f36cdac
alexander weiss <42820958+artorius611@users.noreply.github.com>
2019-01-30 14:31:01 -0600

    Update treasure_generator.py

--- treasure_generator.py
+++ treasure_generator.py
@@ -4,11 +4,35 @@ import Permanent_Role
 
 party = input('How many people are in the party?   ')
 level = input('What is the Level of the party?   ')
-if int(party) >= 4 :
-    if int(level) == 1 :
-        Consumable_Role.Level_one()
-        Permanent_Role.Level_one()
+base_party = 4
+gold = 0
+
+if int(level) == 1 :
+    Consumable_Role.Level_one()
+    Permanent_Role.Level_one()
+    gold = 32
+    while base_party < int(party) :
+        base_party += 1
+        gold += 8
+        Consumable_Role.Level_one_extra()
+        Permanent_Role.Level_one_extra()
     else :
-        print (level, 'is not a correct level')
+        print (Consumable_Role.Con_treasure)
+        print (Permanent_Role.Perm_treasure)
+        print ('Gold :', gold)
+        
+elif int(level) == 2 :
+    Consumable_Role.Level_two()
+    Permanent_Role.Level_two()
+    gold = 64
+    while base_party < int(party) :
+        base_party += 1
+        gold += 16
+        Consumable_Role.Level_two_extra()
+        Permanent_Role.Level_two_extra()
+    else :
+        print (Consumable_Role.Con_treasure)
+        print (Permanent_Role.Perm_treasure)
+        print ('Gold :', gold)
 else :
-    print ('you got a big party')
+    print ('Your Level is too High')
