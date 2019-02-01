77297a44e16853642e0ebc3f7ac64b21f79da977
alexander weiss <42820958+artorius611@users.noreply.github.com>
2019-02-01 16:01:06 -0600

    Update treasure_generator.py

--- treasure_generator.py
+++ treasure_generator.py
@@ -21,7 +21,6 @@ if int(level) == 1 :
         print (Consumable_Role.Con_treasure)
         print (Permanent_Role.Perm_treasure)
         print ('Gold :', gold)
-
 elif int(level) == 2 :
     Consumable_Role.Level_two()
     Permanent_Role.Level_two()
@@ -35,5 +34,109 @@ elif int(level) == 2 :
         print (Consumable_Role.Con_treasure)
         print (Permanent_Role.Perm_treasure)
         print ('Gold :', gold)
+elif int(level) == 3 :
+    Consumable_Role.Level_three()
+    Permanent_Role.Level_three()
+    gold = 120
+    while base_party < int(party) :
+        base_party += 1
+        gold += 30
+        Consumable_Role.Level_three_extra()
+        Permanent_Role.Level_three_extra()
+    else :
+        print (Consumable_Role.Con_treasure)
+        print (Permanent_Role.Perm_treasure)
+        print ('Gold :', gold)
+elif int(level) == 4 :
+    Consumable_Role.Level_four()
+    Permanent_Role.Level_four()
+    gold = 200
+    while base_party < int(party) :
+        base_party += 1
+        gold += 50
+        Consumable_Role.Level_four_extra()
+        Permanent_Role.Level_four_extra()
+    else :
+        print (Consumable_Role.Con_treasure)
+        print (Permanent_Role.Perm_treasure)
+        print ('Gold :', gold)
+elif int(level) == 5 :
+    Consumable_Role.Level_five()
+    Permanent_Role.Level_five()
+    gold = 320
+    while base_party < int(party) :
+        base_party += 1
+        gold += 80
+        Consumable_Role.Level_five_extra()
+        Permanent_Role.Level_five_extra()
+    else :
+        print (Consumable_Role.Con_treasure)
+        print (Permanent_Role.Perm_treasure)
+        print ('Gold :', gold)
+elif int(level) == 6 :
+    Consumable_Role.Level_six()
+    Permanent_Role.Level_six()
+    gold = 500
+    while base_party < int(party) :
+        base_party += 1
+        gold += 125
+        Consumable_Role.Level_six_extra()
+        Permanent_Role.Level_six_extra()
+    else :
+        print (Consumable_Role.Con_treasure)
+        print (Permanent_Role.Perm_treasure)
+        print ('Gold :', gold)
+elif int(level) == 7 :
+    Consumable_Role.Level_seven()
+    Permanent_Role.Level_seven()
+    gold = 720
+    while base_party < int(party) :
+        base_party += 1
+        gold += 180
+        Consumable_Role.Level_seven_extra()
+        Permanent_Role.Level_seven_extra()
+    else :
+        print (Consumable_Role.Con_treasure)
+        print (Permanent_Role.Perm_treasure)
+        print ('Gold :', gold)
+elif int(level) == 8 :
+    Consumable_Role.Level_eight()
+    Permanent_Role.Level_eight()
+    gold = 1000
+    while base_party < int(party) :
+        base_party += 1
+        gold += 250
+        Consumable_Role.Level_eight_extra()
+        Permanent_Role.Level_eight_extra()
+    else :
+        print (Consumable_Role.Con_treasure)
+        print (Permanent_Role.Perm_treasure)
+        print ('Gold :', gold)
+elif int(level) == 9 :
+    Consumable_Role.Level_nine()
+    Permanent_Role.Level_nine()
+    gold = 1400
+    while base_party < int(party) :
+        base_party += 1
+        gold += 350
+        Consumable_Role.Level_nine_extra()
+        Permanent_Role.Level_nine_extra()
+    else :
+        print (Consumable_Role.Con_treasure)
+        print (Permanent_Role.Perm_treasure)
+        print ('Gold :', gold)
+elif int(level) == 10 :
+    Consumable_Role.Level_ten()
+    Permanent_Role.Level_ten()
+    gold = 2000
+    while base_party < int(party) :
+        base_party += 1
+        gold += 500
+        Consumable_Role.Level_ten_extra()
+        Permanent_Role.Level_ten_extra()
+    else :
+        print (Consumable_Role.Con_treasure)
+        print (Permanent_Role.Perm_treasure)
+        print ('Gold :', gold)                                        
 else :
     print ('Your Level is too High')
